79e175fa742526d23a785db44cc9ef787bd6ab3d
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-12-25 12:57:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
-    //Write your code here
+    // Get the form element
+    var form = document.getElementById('form1');
 
+    // Access the input fields by name
+    var firstName = form.elements['fname'].value;
+    var lastName = form.elements['lname'].value;
+
+    // Display the first and last name using alert
+    alert(firstName +" "+ lastName);
+
+    // Prevent the default form submission
+    return false;
 }
